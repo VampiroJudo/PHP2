4ff70753dd1cc116151d62ccdbcfe8c42723d50c
John <johnconley507@hotmail.com>
2017-04-05 20:23:43 -0400

    working with values PHP

---
+++ PHPFundamentals/GeneralTypes/Booleans.php
@@ -0,0 +1,18 @@
+<?php
+
+$bool = true;
+
+$bool2 = false;
+
+$hasValue = 1234;
+
+$hasValue = "";
+
+
+var_dump($bool);
+var_dump($bool2);
+var_dump((bool)$hasValue);
+var_dump((bool)$hasValue);
+
+
+?>
---
+++ PHPFundamentals/GeneralTypes/FloatingPoints.php
@@ -0,0 +1,13 @@
+<?php
+
+$float = 1.234;
+
+$scientific = 0.1234E4;
+
+$scientific2=1234E-4;
+
+var_dump($scientific2);
+var_dump($scientific);
+var_dump($float);
+
+?>
