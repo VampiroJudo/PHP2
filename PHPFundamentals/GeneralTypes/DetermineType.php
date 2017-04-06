27ee556d38d89efad54d4c05809140e1807752ca
John <johnconley507@hotmail.com>
2017-04-05 21:26:57 -0400

    learning to define types

--- PHPFundamentals/GeneralTypes/DetermineType.php
+++ PHPFundamentals/GeneralTypes/DetermineType.php
@@ -8,4 +8,12 @@ $stringVar = "I'm a string";
 $boolVar = false;
 $floatVar = 12.34;
 
+echo is_int($intVar);
+echo is_string($stringVar);
+echo is_bool($boolVar);
+echo is_float($floatVar);
+
+echo defined('CHECK_CONSTANT');
+
+//if variable is the correct type printout will be a 1/ if not, it prints nothing.
 ?>
