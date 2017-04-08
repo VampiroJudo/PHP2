e4307e97162a12fed21b3800a5f06f50d9565525
John <johnconley507@hotmail.com>
2017-04-07 20:21:14 -0400

    more PHP array functions

--- PHPFundamentals/Arrays/array_push.php
+++ PHPFundamentals/Arrays/array_push.php
@@ -2,8 +2,12 @@
 
 $authors = array("Charles Dickens", "Jane Austin", "William Shakespeare");
 
-array_push($authors, "Lousia May Alcott");
+array_push($authors, "Louisa May Alcott");// Will add "Louisa May Alcott" to the array
+
+$authors[] = "L.M. Montgomery";// preferred method to add an element to an array
+
+//array_push only adds index values
 
 
-
+print_r($authors);
 ?>
